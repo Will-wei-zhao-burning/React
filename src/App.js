a1dd1859bd176686746eb1df297de9a8de56a19d
Will <775817124@qq.com>
2021-03-12 11:21:31 +0800

    hooks-EffectHook的使用

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@
  * @Author: BurNing
  * @Date: 2021-03-12 10:25:35
  * @LastEditors: BurNing
- * @LastEditTime: 2021-03-12 10:26:04
+ * @LastEditTime: 2021-03-12 11:16:54
  */
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import './App.css';
-/* useEffectHook */
+/* useEffectHook  */
 function App() {
-  
+  const [counter, setcounter] = useState(0);
+  const [status, setstatus] = useState(0);
+  useEffect(() => {
+      document.title = `you clicked${counter} times`;
+      return()=>{
+        setstatus(status+2);
+      }
+  }, [counter,status])
+  return(
+      <div>
+          <div>Clicked:{counter}</div>
+          <div>Status:{status}</div>
+          <button onClick={()=>setcounter(counter+1)}>+1</button>
+      </div>
+  )
 }
 
 export default App;
